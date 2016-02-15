54bda82fa86c8c4be08163c093ccde941f9102d2
Solvek <sa@mobiwolf.com>
2016-02-15 15:06:34 +0200

    Video playing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -21,6 +21,7 @@ def MainMenu():
 
 	oc.add(VideoClipObject(
 		url = 'http://videoprobki.ua/uk/camera/188-rozvyazka-prosp-bazhana-%D1%96-dn%D1%96provsko%D1%97-nab-metro-osokorki?c=Kyiiv',
+		# url = 'http://vs7.videoprobki.com.ua/streams/cam188stream_1455527504.mp4',
 		title = 'Osokorky',
 		summary = 'Some summary',
 		thumb = Callback(Thumb, url='http://maps.googleapis.com/maps/api/staticmap?center=50.3952,30.6184&zoom=15&size=300x150&markers=icon:http://videoprobki.ua/themes/videoprobki/images/icons/green_45_active.gif|50.3952,30.6184&sensor=true&language=uk')
