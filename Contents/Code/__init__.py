e4cec1e2755b39c2a4c3f0d5f6fb492ab6f7818e
Solvek <sa@mobiwolf.com>
2016-02-19 14:59:45 +0200

    Menu hierarchy

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,7 +5,11 @@ ICON   = 'icon-default.png'
 
 PREFIX = '/video/videoprobki'
 
-####################################################################################################
+BASE_URL = 'http://mobile.videoprobki.ua%s'
+
+PARSED = 'parsed'
+
+################################################
 def Start():
 
 	ObjectContainer.title1 = TITLE
@@ -13,27 +17,164 @@ def Start():
 	DirectoryObject.art = R(ART)
 	DirectoryObject.thumb = R(ICON)
 
-####################################################################################################
+	Data.Remove(PARSED)
+
+	Dict['city'] = None
+
+#################################################
 @handler(PREFIX, TITLE)
+@route(PREFIX+'/cities/city')
 def MainMenu():
+	currentCity = Dict['city']
 
-	oc = ObjectContainer()
+	# Log.Debug('Current city: '+currentCity)
 
-	oc.add(VideoClipObject(
-		url = 'http://videoprobki.ua/uk/camera/188-rozvyazka-prosp-bazhana-%D1%96-dn%D1%96provsko%D1%97-nab-metro-osokorki?c=Kyiiv',
-		# url = 'http://vs7.videoprobki.com.ua/streams/cam188stream_1455527504.mp4',
-		title = 'Osokorky',
-		summary = 'Some summary',
-		thumb = Callback(Thumb, url='http://maps.googleapis.com/maps/api/staticmap?center=50.3952,30.6184&zoom=15&size=300x150&markers=icon:http://videoprobki.ua/themes/videoprobki/images/icons/green_45_active.gif|50.3952,30.6184&sensor=true&language=uk')
-	))
+	if not currentCity:
+		return MenuCities()
+
+	oc = MenuCity(currentCity)
+
+	oc.add(DirectoryObject(
+		            key = Callback(MenuCities),
+		            title = L("Select city")
+		        ))
 
 	return oc
 
-####################################################################################################
-def Thumb(url):
+################################################
+@route(PREFIX+'/cities')
+def MenuCities():
+	oc = ObjectContainer()
 
-  try:
-    data = HTTP.Request(url, cacheTime = CACHE_1MONTH).content
-    return DataObject(data, 'image/jpeg')
-  except:
-    return Redirect(R(ICON))
+	for cityName in GetContent().keys():
+		# cityName = cityName.decode()
+		# Log.Debug('Listing cities, city: '+cityName)
+		oc.add(DirectoryObject(
+		            key = Callback(MenuCity, city = cityName),
+		            title = cityName
+		        ))
+
+	return oc
+
+################################################
+@route(PREFIX+'/cities/{city}')
+def MenuCity(city='dddd'):
+	Log.Debug("Menu for city "+city)
+	Dict['city'] = city
+
+	if city == 'Київ':
+		return CityDistricts(city)
+
+	return CityCameras(city)
+
+################################################
+@route(PREFIX+'/cities/{city}/{district}')
+def MenuDistrict(city, district):
+	oc = ObjectContainer()
+
+	district = GetContent().get(city).get(district)
+
+	AppendDistrict(oc, district)
+
+	return oc
+
+################################################
+@route(PREFIX+'/exact/{city}/districts')
+def CityDistricts(city):
+	oc = ObjectContainer()
+
+	districts = GetContent().get(city)
+
+	# Log.Debug('For city %s, districts: %s' % (city, JSON.StringFromObject(districts)))
+
+	for districtName in districts.keys():
+		oc.add(DirectoryObject(
+		            key = Callback(MenuDistrict, city=city, district = districtName),
+		            title = districtName
+		        ))
+
+	return oc
+
+################################################
+@route(PREFIX+'/exact/{city}/cameras')
+def CityCameras(city):
+	oc = ObjectContainer()
+
+	districts = GetContent().get(city)
+	for district in districts.values():
+		AppendDistrict(oc, district)
+
+	return oc
+
+################################################
+def AppendDistrict(oc, district):
+	if not district:
+		return
+
+	for camera in district:
+		Log.Debug('Camera url: %s' % camera.get('url'))
+		oc.add(VideoClipObject(
+			url = camera.get('url'),
+			title = camera.get('place')+" "+camera.get('direction'),
+			summary = camera.get('city')+", "+camera.get('district')
+		))
+
+################################################
+def GetContent():
+	if Data.Exists(PARSED):
+		return Data.LoadObject(PARSED)
+
+	parsed = {}
+
+	content = HTML.ElementFromURL(BASE_URL % '/uk/all-cameras', encoding='utf-8')
+
+	for camera in content.xpath('//div[contains(@class, "cam-info")]/div[2]'):
+		url = BASE_URL % camera.xpath('.//a')[0].get('href')
+
+		parts = [extractValue(part) for part in HTML.StringFromElement(camera).decode().split('<br>')]
+
+		city = parts[0]
+		district = parts[1]
+		place = parts[2]
+		direction = parts[3]
+
+		# Log.Debug('Url: %s, city: "%s", district: %s, place: %s, direction: %s' % (url, city, district, place, direction))
+
+		cityItems = parsed.get(city)
+
+		if not cityItems:
+			cityItems = {}
+			parsed[city] = cityItems
+
+		districtItems = cityItems.get(district)
+
+		if not districtItems:
+			districtItems = []
+			cityItems[district] = districtItems
+
+		districtItems.append({
+		                     'city': city,
+		                     'district': district,
+		                     'place':place,
+		                     'direction':direction,
+		                     'url':url})
+
+	Data.SaveObject(PARSED, parsed)
+
+	# Log.Debug('Whole content %s' % JSON.StringFromObject(parsed))
+
+	return parsed
+
+def extractValue(s):
+	s = String.StripTags(s)
+	f = s.find(': ')
+	return s[f+2:].strip()
+
+################################################
+# def Thumb(url):
+
+#   try:
+#     data = HTTP.Request(url, cacheTime = CACHE_1MONTH).content
+#     return DataObject(data, 'image/jpeg')
+#   except:
+#     return Redirect(R(ICON))
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Traffic jam videos from page videoprobki.ua (Ukraine) for Plex.tv
 
 # Developers info
+ * Cameras for city in json: http://videoprobki.com.ua/tmp/mralex/get.php?city=10
  * Request for current stream `curl 'http://videoprobki.ua/gplmod7' -H 'Content-Type: application/x-www-form-urlencoded'  --data 'p1=cam188&p2=2&p3=1&p4=1'`
+ *
 
 http://vs7.videoprobki.com.ua/streams/cam188stream_1455549310.mp4
